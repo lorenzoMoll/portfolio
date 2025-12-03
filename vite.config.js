50fc0cf91145196520de06ae204502d566d19610
Lorenzo <llorensmoll89@gmail.com>
2025-12-03 13:51:36 +0100

    Test Images

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,20 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/portfolio/'
+  base: '/portfolio/',
+  build: {
+    // Añade esta configuración para assets
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'imgs';
+          }
+          return `${extType}/[name]-[hash][extname]`;
+        },
+      }
+    }
+  }
 })
